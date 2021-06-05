ef87eda851bb48887bb888246b3add2274bbb504
Nick <wrignj08@gmail.com>
2021-06-05 20:10:36 +0800

    moved to manifest v3

--- background.js
+++ background.js
@@ -1,5 +1,7 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
 
-   chrome.tabs.executeScript(null, {file: "redirect.js"});
-
-});
+chrome.action.onClicked.addListener(function(tab) {
+   chrome.scripting.executeScript({
+    target: {tabId: tab.id},
+    files: ['redirect.js'],
+  });
+})
